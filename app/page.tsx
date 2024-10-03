c0be2ebde7049ac2934db68c30aef8dc9d84bc39
ddmilado <aestrumlabs@gmail.com>
2024-10-03 21:55:03 +0400

    first1

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-5 row-start-2 items-center sm:items-start">
         <h2>Enter New Password</h2> {/* Header for the form */}
         {/* Form for entering new password */}
         <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
